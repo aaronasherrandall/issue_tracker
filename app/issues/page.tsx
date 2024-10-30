c8d2dc85e9ba99b847ee1221ff4b6062b5a971b4
Aaron Randall <aaronasherrandall@gmail.com>
2024-10-30 09:09:11 -0400

    radix ui setup

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 // Importing the React library. This is required when using JSX, as React is needed to convert JSX into standard JavaScript.
 
 const IssuePage = () => {
   // Defining a functional component named 'page'. In React, functional components are functions that return JSX.
   // This component has no props or state in this simple example.
 
-  return <div>Issue Page</div>;
+  return (
+    <div>
+      <Button>New Issue</Button>
+    </div>
+  );
   // JSX (JavaScript XML): A syntax extension used by React that looks similar to HTML.
   // This is returning a <div> element with the text "page". The <div> will be rendered on the UI when this component is used.
 };
